7a22593fed582e9b288e20aec8c6f37a693adf60
Sazzad Hossain <sharifh530@gmail.com>
2020-08-10 22:43:08 +0600

    Alan intro of the project implemented

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import "./App.css";
 
 import alanBtn from "@alan-ai/alan-sdk-web";
 
-const alankey = "";
+const alankey =
+  "a6116ac63672c4b5499c04708cc5d4272e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 function App() {
   useEffect(() => {
     alanBtn({
       key: alankey,
+      onCommand: ({ command }) => {
+        if (command === "testCommand") {
+          alert("this code was executed");
+        }
+      },
     });
   }, []);
 
