22e1778805bd3d8796ec429683a66693fb604889
Sazzad Hossain <sharifh530@gmail.com>
2020-08-10 22:30:44 +0600

    Initialize AI-News-Application with create-reate-app. Add the dependecy of alan-ai, material-ui/core, classnames and words-to-numbers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
+
+import alanBtn from "@alan-ai/alan-sdk-web";
+
+const alankey = "";
 
 function App() {
+  useEffect(() => {
+    alanBtn({
+      key: alankey,
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Alan AI News Application</h1>
     </div>
   );
 }
